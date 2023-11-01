b905bb283d87004ebadcee6f3ea765ce829f9091
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2023-11-01 11:28:50 +0600

    image inserted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Gallery from "./components/Gallery";
+import {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+} from "./assets/images/index";
+
 function App() {
-  return <div className="m-32"></div>;
+  const images = [
+    { id: "1", src: img1, alt: "Image 1" },
+    { id: "2", src: img2, alt: "Image 2" },
+    { id: "3", src: img3, alt: "Image 3" },
+    { id: "4", src: img4, alt: "Image 4" },
+    { id: "5", src: img5, alt: "Image 5" },
+    { id: "6", src: img6, alt: "Image 6" },
+    { id: "7", src: img7, alt: "Image 7" },
+    { id: "8", src: img8, alt: "Image 8" },
+    { id: "9", src: img9, alt: "Image 9" },
+    { id: "10", src: img10, alt: "Image 10" },
+    { id: "11", src: img11, alt: "Image 11" },
+  ];
+
+  return (
+    <div>
+      <Gallery images={images} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-export const Gallery = () => {
-  return <div>Gallery</div>;
+const Gallery = ({ images }) => {
+  console.log(images[0]);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [featureImage, setFeatureImage] = useState(images[0].id);
+
+  const handleSelect = (id) => {
+    if (selectedImages.includes(id)) {
+      setSelectedImages(selectedImages.filter((imageId) => imageId !== id));
+    } else {
+      setSelectedImages([...selectedImages, id]);
+    }
+  };
+
+  const handleDelete = () => {
+    // Implement logic to delete selected images
+    console.log("Deleting images:", selectedImages);
+    setSelectedImages([]);
+  };
+
+  const handleDragEnd = (result) => {
+    // Implement logic to reorder images
+    if (!result.destination) {
+      return;
+    }
+
+    const updatedImages = [...images];
+    const [reorderedImage] = updatedImages.splice(result.source.index, 1);
+    updatedImages.splice(result.destination.index, 0, reorderedImage);
+
+    // Update the state with the new image order
+    // You may also need to update featureImage if it has changed
+    console.log("Reordered images:", updatedImages);
+  };
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="gallery" direction="horizontal">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="flex space-x-4"
+            >
+              {images.map((image, index) => (
+                <Draggable key={image.id} draggableId={image.id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className={`relative rounded-lg ${
+                        image.id === featureImage
+                          ? "border-2 border-blue-500"
+                          : ""
+                      }`}
+                    >
+                      <input
+                        type="checkbox"
+                        checked={selectedImages.includes(image.id)}
+                        onChange={() => handleSelect(image.id)}
+                        className="absolute top-2 right-2"
+                      />
+                      <img src={image.src} alt={image.alt} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <button onClick={handleDelete} className="btn">
+        Delete
+      </button>
+    </div>
+  );
 };
+
+export default Gallery;
